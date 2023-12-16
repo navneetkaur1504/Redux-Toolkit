88698683557b57552f6d5f758c2941040530e5ab
navneetkaur1504 <navneetkaur150403@gmail.com>
2023-12-17 00:12:09 +0530

    updated

---
+++ redux/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+
+function App() {
+  return (
+    <div classname="App"> 
+    <BrowserRouter></BrowserRouter>
+    
+    </div>
+
+
+  );
+}
+
+export default App;
