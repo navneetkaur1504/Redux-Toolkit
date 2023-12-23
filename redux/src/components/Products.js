36ee379d9dd401e52e61d0030ac507b6028265f6
navneetkaur1504 <navneetkaur150403@gmail.com>
2023-12-23 21:04:46 +0530

    updated store

--- redux/src/components/Products.js
+++ redux/src/components/Products.js
@@ -8,13 +8,24 @@ const Products = () => {
             const res = await fetch('https://fakestoreapi.com/products');
             const data = await res.json();
             console.log(data);
-
-        }
+            setProducts(data);
+        };
         fetchProducts();
 
     }, []);
   return (
-    <div>Products</div>
+    <div className='productsWrapper'>
+        {
+          products.map((product) => (
+          <div className='card' key={product.id}>
+            <img src = {product.image} alt="" />
+            <h4>{product.title}</h4>
+            <h5>{product.price}</h5>
+            <button className='btn'>Add to Cart</button>
+          </div>
+           ))
+        }
+    </div>
   )
 }
 
