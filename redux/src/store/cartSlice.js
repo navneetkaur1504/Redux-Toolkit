479bab82a59c7f1ccfe4c06db1b371cac4382db5
navneetkaur1504 <navneetkaur150403@gmail.com>
2023-12-24 21:18:22 +0530

    update

--- redux/src/store/cartSlice.js
+++ redux/src/store/cartSlice.js
@@ -1,16 +1,17 @@
-const {createSlice} = require("@reduxjs/toolkit");
-
-const initialState = [];
+const { createSlice } = require('@reduxjs/toolkit');
 
 const cartSlice = createSlice({
     name: 'cart',
-    initialState,
+    initialState: [],
     reducers: {
         add(state, action) {
-            
             state.push(action.payload);
-
         },
-        remove() {},
+        remove(state, action) {
+            return state.filter((item) => item.id !== action.payload);
+        },
     },
-})
+});
+
+export const { add, remove } = cartSlice.actions;
+export default cartSlice.reducer;
